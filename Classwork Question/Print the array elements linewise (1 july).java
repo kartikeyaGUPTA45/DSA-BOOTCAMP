b84e8403444e27965140c93918e26431fc1f4d5d
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2023-07-01 22:38:07 +0530

    Create Print the array elements linewise (1 july).java

---
+++ Classwork Question/Print the array elements linewise (1 july).java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    
+    public static void printArrayElements(int arr[]){
+        // int n =arr.length;
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        
+        int arr [] = new int[n];
+        
+//         User Input for array
+        for(int i = 0;i<n;i++){
+            arr[i] = scn.nextInt();
+        }
+        
+        printArrayElements(arr);
+    }
+}
