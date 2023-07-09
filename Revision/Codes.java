3c340ec6c55de31df6a69b5e563f50080492de3f
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2023-07-09 13:15:06 +0530

    Update Codes.java

--- Revision/Codes.java
+++ Revision/Codes.java
@@ -1,1 +1,110 @@
+import java.io.*;
+import java.util.*;
 
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+//         System.out.println("*****");
+//         System.out.println("   *");
+//         System.out.print("  *\n");
+//         System.out.print(" *\n");
+//         System.out.println("*****");
+        
+        
+//         int a = 23;
+//         long b = 12324124;
+//         String str = "NavGurukul Bootcamp";
+//         char ch = 'a';
+//         boolean e = true;
+//         float f = 12.34f;
+//         double d = 12.345324;
+        
+//         System.out.println(a);
+//         System.out.println(b);
+//         System.out.println(str);
+//         System.out.println(ch);
+//         System.out.println(!e);
+//         System.out.println(f);
+//         System.out.println(d);
+        
+//         Scanner scn = new Scanner(System.in);
+//         int a = scn.nextInt();
+//         int e =scn.nextInt();
+//         boolean b = scn.nextBoolean();
+//         double d =scn.nextDouble();
+        
+//         + " "
+//             String.valueOf(a)
+            
+//             char ch = scn.next().charAt(0);
+        
+        
+//         System.out.println("Integer value is: " + (a+e));
+//         // System.out.println("Boolean value is: " + b);
+//         // System.out.println("Decimal value is: " + d);
+        
+//         int age =24;
+        
+//         if(age>18){
+//             System.out.println("Eligible for voting");
+//         }else{
+//             System.out.println("Not Eligible for voting");
+//         }
+        
+        
+//         int marks = 70;
+        
+//         if (marks>90){
+//             System.out.println("A");
+//         }
+//         else if(marks> 80){
+//             System.out.println("B");
+//         }
+//         else if(marks>70){
+//             System.out.println("C");
+//         }
+//         else if(marks>60){
+//             System.out.println("D");
+//         }
+//         else{
+//             System.out.println("Fail");
+//         }
+        
+        
+//          int marks = 87;
+        
+//         if (marks>90){
+//             if(marks>95)
+//                 System.out.println("A1");
+//             else
+//                 System.out.println("A2");
+//         }
+//         else if(marks> 80){
+//             if(marks>85)
+//                 System.out.println("B1");
+//             else
+//                 System.out.println("B2");
+//         }
+//         else if(marks>70){
+//             System.out.println("C");
+//         }
+//         else if(marks>60){
+//             System.out.println("D");
+//         }
+//         else{
+//             System.out.println("Fail");
+//         }
+        
+        // for(int i=1;i<=2000;i++){
+        //     System.out.println("*");
+        // }
+        
+        int i=1;
+        while(i<=10){
+            System.out.println("*");
+            i++;
+        }
+        
+    }
+}
