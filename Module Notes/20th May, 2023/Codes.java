0c63d2b2ccee699c1044ad4bf18a2c4e83ef35ff
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2023-07-24 03:42:32 +0530

    Create Codes.java

---
+++ Module Notes/20th May, 2023/Codes.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    
+    public static void main(String[] args) {
+        
+        System.out.print("My name is ABC \n" );
+        System.out.print("I am working in NavGurukul");
+        
+        System.out.println("*****");
+        System.out.println("   *");
+        System.out.println("  *");
+        System.out.println(" *");
+        System.out.println("*****");
+        
+        System.out.print("*   *\n");
+        System.out.print(" * *\n");
+        System.out.print("  *\n");
+        System.out.print(" * *\n");
+        System.out.print("*   *");
+        
+        boolean a = true;
+        boolean b = false;
+        boolean c = false;
+        
+        System.out.println(!a);
+        
+        int a = 10, b = 15;
+        
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+        boolean a = true;
+        System.out.println(a);
+        
+        boolean b = false;
+        System.out.println(b);
+        
+        boolean a = true, b = false;
+        System.out.println(a);
+        System.out.println(b);
+        
+        System.out.println(a + " "+ b);
+        
+        
+        String str = "SKDNGKDNGSKGN";
+        System.out.println(str);
+        
+        String str1 = null;
+        System.out.println(str1);
+        
+        int $abc = 953;
+        System.out.println($abc);
+        
+        char ch = 'I am a student';
+        System.out.println(str);
+
+    } 
+}
