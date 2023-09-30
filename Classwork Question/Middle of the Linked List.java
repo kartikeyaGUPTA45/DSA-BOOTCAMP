a6e717672de7dd370bc2522bff4b93c85c450916
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2023-09-30 23:21:46 +0530

    Create Middle of the Linked List.java

---
+++ Classwork Question/Middle of the Linked List.java
@@ -0,0 +1,17 @@
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        return slow;    
+    }
+}
